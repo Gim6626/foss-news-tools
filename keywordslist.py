3cc515a606edb82e9adeb2a6ad1226fb33b36c3f
Dmitriy Vinokurov <gim6626@gmail.com>
2021-05-21 21:18:08 +0500

    Keywords list for server generator update after recent keywords structure fixes

--- keywordslist.py
+++ keywordslist.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
-from data.digestrecordsubcategorykeywords import DIGEST_RECORD_SUBCATEGORY_KEYWORDS_MAPPING
 from data.generalkeywords import GENERAL_KEYWORDS
 from data.lfkeywords import LF_KEYWORDS
 import yaml
+import os
 
 keywords = {
     'generic': [],
@@ -11,11 +11,18 @@ keywords = {
 }
 keywords['specific'] += GENERAL_KEYWORDS
 keywords['specific'] += LF_KEYWORDS
-for subcategory, subcategory_keywords in DIGEST_RECORD_SUBCATEGORY_KEYWORDS_MAPPING.items():
+
+subcategories_keywords_path = os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                                           'data',
+                                           'digestrecordsubcategorykeywords.yaml')
+with open(subcategories_keywords_path, 'r') as fin:
+    subcategories_keywords = yaml.safe_load(fin)
+
+for subcategory, subcategory_keywords in subcategories_keywords.items():
     for keywords_type in ('generic', 'specific'):
         keywords[keywords_type] += subcategory_keywords[keywords_type]
 
 for keywords_type in ('generic', 'specific'):
     keywords[keywords_type] = sorted(list(set(keywords[keywords_type])))
 
-print(yaml.dump(keywords))
+print(yaml.dump(keywords, allow_unicode=True))
