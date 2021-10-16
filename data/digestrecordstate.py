1bb3b9a822c343558cf97e67a03ba54a7c0fb43e
Dmitriy Vinokurov <gim6626@gmail.com>
2021-10-16 14:56:19 +0500

    Added "duplicate" digest record state after recent server changes

--- data/digestrecordstate.py
+++ data/digestrecordstate.py
@@ -4,6 +4,7 @@ class DigestRecordState(Enum):
     UNKNOWN = 'unknown'
     IN_DIGEST = 'in_digest'
     OUTDATED = 'outdated'
+    DUPLICATE = 'duplicate'
     IGNORED = 'ignored'
 
 DIGEST_RECORD_STATE_VALUES = [state.value for state in DigestRecordState]
